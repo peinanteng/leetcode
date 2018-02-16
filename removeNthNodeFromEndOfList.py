4f01b0ba30300eef8f731ba411ae8d839ffed4dd
Peinan Teng <tengpn@gmail.com>
2018-02-16 09:50:02 -0800

    Modify

--- removeNthNodeFromEndOfList.py
+++ removeNthNodeFromEndOfList.py
@@ -11,28 +11,20 @@ class Solution(object):
         :type n: int
         :rtype: ListNode
         """
-        if head is None or n == 0:
+        if head == None:
             return head
-        if n == 1 and head.next == None:
-            return None
         cur = head
-        curlen = 0
+        count = 0
         while cur:
-            curlen += 1
+            count += 1
             cur = cur.next
-        m = curlen - n + 1
-        
-        if m == 1:
-            head = head.next
-        pre = head
-        cur = head.next
-        count = 2
-        while count <= m:
-            if count == m:
-                pre.next = cur.next
-                break
-            else:
-                pre, cur = cur, cur.next
-                count += 1
+        m = count - n
+        if m == 0:
+            return head.next
+        count = 1
+        cur = head
+        while count < m:
+            cur = cur.next
+            count += 1
+        cur.next = cur.next.next
         return head
-        
