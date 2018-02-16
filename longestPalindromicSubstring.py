af0239271e04a45c7becbf702c2641ad2f3327a7
Peinan Teng <tengpn@gmail.com>
2018-02-16 07:56:53 -0800

    modified longestPalindromicSubstring.py

--- longestPalindromicSubstring.py
+++ longestPalindromicSubstring.py
@@ -1,13 +1,24 @@
-if len(s) == 0:
-            return 0
-        start = 0
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+        res  = ''
+        if len(s) == 0:
+            return res
+        res = s[0]
         maxlen = 1
+        if len(s) == 0:
+            return res
         for i in range(1, len(s)):
-            if i - maxlen >= 1 and s[i - maxlen - 1 : i + 1] == s[i - maxlen - 1: i + 1][::-1]:
-                start = i - maxlen - 1
+            if i - maxlen - 1 >= 0 and s[i - maxlen - 1 : i + 1] == s[i - maxlen - 1 : i + 1][::-1]:
+                res = s[i - maxlen - 1 : i + 1]
                 maxlen += 2
-                continue
-            if i - maxlen >= 0 and s[i - maxlen : i + 1] == s[i - maxlen: i + 1][::-1]:
-                start = i - maxlen
+            elif i - maxlen >= 0 and s[i - maxlen : i + 1] == s[i - maxlen : i + 1][::-1]:
+                res = s[i - maxlen : i + 1]
                 maxlen += 1
-        return s[start : start + maxlen]
+        return res
+            
+      
