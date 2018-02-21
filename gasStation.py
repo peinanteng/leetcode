19d73cfabf3e9cd9e2bc77516e5bbb40c595d713
Peinan Teng <tengpn@gmail.com>
2018-02-20 17:28:48 -0800

    Modified gasStation.py

--- gasStation.py
+++ gasStation.py
@@ -5,24 +5,16 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
-        nums = []
-        for i in range(len(gas)):
-            nums.append(gas[i] - cost[i])
         start = 0
-        # while start < len(nums) and nums[start] < 0:
-        #     start += 1
-        # minres = sum(nums[:start])
+
         minres = 2 ** 31
         cursum = 0
-        summary = {}
-        for i in range(len(nums)):
-            cursum += nums[i]
-            if cursum not in summary:
-                summary[cursum] = i
+        for i in range(len(gas)):
+            cursum += gas[i] - cost[i]
             if cursum < minres:
                 minres = cursum
                 start = i
-        if start == len(nums) - 1:
+        if start == len(gas) - 1:
             start = 0
         else:
             start += 1
