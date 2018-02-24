a021599a3e2a72be3f726ba9c0f037ac2307dee1
Peinan Teng <tengpn@gmail.com>
2018-02-23 17:18:38 -0800

    modified minPathSum.py

--- minPathSum.py
+++ minPathSum.py
@@ -5,21 +5,16 @@ class Solution(object):
         :rtype: int
         """
         m, n = len(grid), len(grid[0])
-        matrix = [[0] * n for i in range(m)]
         
-        x, y = 0, 0
-        matrix[0][0] = grid[0][0]
-        y = 1
+        x, y = 0, 1
         while y < n:
-            matrix[x][y] = matrix[x][y - 1] + grid[x][y]
+            grid[x][y] += grid[x][y - 1]
             y += 1
         x, y = 1, 0
         while x < m:
-            matrix[x][y] = matrix[x - 1][y] + grid[x][y]
+            grid[x][y] += grid[x - 1][y]
             x += 1
         for x in range(1, m):
             for y in range(1, n):
-                matrix[x][y] = min(matrix[x - 1][y], matrix[x][y - 1]) + grid[x][y]
-        return matrix[m - 1][n - 1]
-
-
+                grid[x][y] += min(grid[x - 1][y], grid[x][y - 1])
+        return grid[m - 1][n - 1]
