6709c6b4445713f5d34961d7f8499121fed09b06
Peinan Teng <tengpn@gmail.com>
2018-02-26 21:31:44 -0800

    Address comments: replace with camelCase

--- maximumSubarray.py
+++ maximumSubarray.py
@@ -6,9 +6,8 @@ class Solution(object):
         """
         if not nums:
             return 0
-        cursum = nums[0]
-        result = nums[0]
+        curSum = result = nums[0]
         for num in nums[1:]:
-            cursum = max(cursum + num, num)
-            result = max(result, cursum)
+            curSum = max(curSum + num, num)
+            result = max(result, curSum)
         return result
