33ace0d7bc6dd19b708e467bef100f4e815efa80
Peinan Teng <tengpn@gmail.com>
2018-02-28 08:18:38 -0800

    Addressed the comment

--- shortestPalindrome.py
+++ shortestPalindrome.py
@@ -4,12 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        maxlen = 1
+        maxLen = 1
         for i in range(1, len(s)):
             if s[:i + 1] == s[:i + 1][::-1]:
-                maxlen = i + 1
-        Addedstring = ''
-        if maxlen < len(s):
-            Addedstring = s[maxlen :][::-1]
+                maxLen = i + 1
+        addedString = ''
+        addedString = s[maxLen:][::-1]
         
-        return Addedstring + s
+        return addedString + s
