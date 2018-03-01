ff4123cc0c5a8a62abc5351d11aa3d72c956f544
Peinan Teng <tengpn@gmail.com>
2018-03-01 09:40:29 -0800

    modified containerWithMostWater.py

--- containerWithMostWater.py
+++ containerWithMostWater.py
@@ -4,20 +4,13 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        # initilise the result value
-        res = 0
-        # if the array contains less than 2 numbers: input is invalid
-        if len(height) <= 1:
-            return res
-        # use two pointer, one starting from left, one starting from right
-        leftp, rightp = 0, len(height) - 1
-        # loop if left pointer index is less than right pointer index
-        while leftp < rightp:
-            # update the result if current area is larger than last result
-            res = max(res, min(height[leftp], height[rightp]) * (rightp - leftp))
-            # update the pointer, move the pointer whose height is lower
-            if height[leftp] <= height[rightp]:
-                leftp += 1
-            else:
-                rightp -= 1
-        return res
+        maxArea = 0
+        left, right = 0, len(height) - 1
+        while left < right:
+            curArea = min(height[left], height[right]) * (right - left)
+            maxArea = max(maxArea, curArea)
+            newLeft = left + 1 if height[left] <= height[right] else 0
+            newRight = right - 1 if height[left] > height[right] else 0
+            left, right = newLeft, newRight
+        return maxArea
+    
