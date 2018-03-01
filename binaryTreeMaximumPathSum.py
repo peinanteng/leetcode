127f564943f9be1c5ff9578db2757c42a405bda8
Peinan Teng <tengpn@gmail.com>
2018-03-01 13:26:43 -0800

    Add binaryTreeMaximumPathSum.py

---
+++ binaryTreeMaximumPathSum.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return self.helper(root)[2]
+    def helper(self, root):
+        if not root:
+            return [0, 0, - 2**31 - 1]
+        leftVal = self.helper(root.left)
+        rightVal = self.helper(root.right)
+        newLeft = max(leftVal[0], leftVal[1], 0) + root.val
+        newRight = max(rightVal[0], rightVal[1], 0) + root.val
+        newPath = max(leftVal[2], rightVal[2], newLeft + newRight - root.val, newLeft, newRight, root.val)
+        print(newLeft, newRight, newPath)
+        return [newLeft, newRight, newPath]
+        
