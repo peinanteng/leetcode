49ad24345c88322a52d0261701faace989621bf8
Peinan Teng <tengpn@gmail.com>
2018-03-05 21:21:23 -0800

    Modified findTheCelebrity.py

--- findTheCelebrity.py
+++ findTheCelebrity.py
@@ -10,36 +10,27 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        nums = [0] * n
-        # find the candidates who don't know anyone
+        ans = 0
+        # find the celebrity candidate
+        for i in range(1, n):
+            if Celebrity.knows(ans, i):
+                ans = i
+        
+        flag = 1
+        # identify whether somebody don't know the candidate
         for i in range(n):
-            j = 0
-            while j < i:
-                if nums[j] == 0 and knows(j, i):
-                    nums[j] = 1
-                j += 1
-        index = 0
-        # check whether last candidate knows somebody else
-        while index < n - 1 and nums[n - 1] == 0:
-            if knows(n - 1, index):
-                nums[n - 1] = 1
-            index += 1
-        candidates = []
-        for i, num in enumerate(nums):
-            if num == 0:
-                candidates.append(i)
-         # identify whether all other people knows the candidate
-        celebrity = -1
-        for i in candidates:
-            j = 0
-            while j >= 0 and j < n:
-                if j == i:
-                    j += 1
-                elif knows(j, i):
-                    j += 1
-                else:
-                    j = -1
-            if j == n:
-                celebrity = i
-        return celebrity
+            if i != ans and not Celebrity.knows(i, ans):
+                flag = 0
+                break
+        # identify whether candidate knows any others
+        for i in range(n):
+            if i != ans and Celebrity.knows(ans, i):
+                flag = 0
+                break
+        # return the celebrity if it exist, else -1
+        if flag == 1:
+            return ans
+        else:
+            return -1
+                
         
