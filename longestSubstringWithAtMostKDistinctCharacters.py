e513dd1229611cf2210801efcce4cdcd04956b5b
Peinan Teng <tengpn@gmail.com>
2018-03-08 17:41:43 -0800

    Modified

--- longestSubstringWithAtMostKDistinctCharacters.py
+++ longestSubstringWithAtMostKDistinctCharacters.py
@@ -12,7 +12,7 @@ class Solution(object):
         while right < len(s):
             summary[s[right]] = summary.get(s[right], 0) + 1
             while left < len(s) and len(summary) > k:
-                summary[s[left]] = summary.get(s[left], 0) - 1
+                summary[s[left]] -= 1
                 if summary[s[left]] == 0:
                     del summary[s[left]]
                 left += 1
