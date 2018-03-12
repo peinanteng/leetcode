98647e71c64748703fdc05cbcd45ceb1f55911ec
Peinan Teng <tengpn@gmail.com>
2018-03-12 13:16:25 -0700

    Add inorderSuccessorInBST.py

---
+++ inorderSuccessorInBST.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderSuccessor(self, root, p):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root.val < p.val:
+            return self.inorderSuccessor(root.right, p)
+        elif root.val == p.val:
+            return self.findSmallest(root.right)
+        elif self.findLargest(root.left) == p:
+            return root
+        else:
+            return self.inorderSuccessor(root.left, p)
+    
+    def findSmallest(self, root):
+        if not root:
+            return None
+        node = self.findSmallest(root.left)
+        return node if node else root
+        
+    def findLargest(self, root):
+        if not root:
+            return None
+        node = self.findLargest(root.right)
+        return node if node else root
