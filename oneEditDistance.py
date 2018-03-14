d2a98dc5ac18d921c6a96cbf8d11e8ab147fe4aa
Peinan Teng <tengpn@gmail.com>
2018-03-14 14:20:51 -0700

    Add oneEditDistance.py

---
+++ oneEditDistance.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isOneEditDistance(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        m, n = len(s), len(t)
+        if abs(m - n) > 1:
+            return False
+        if m > n:
+            return self.isOneEditDistance(t, s)
+        for i in range(m):
+            if s[i] != t[i]:
+                if m == n:
+                    return s[i + 1:] == t[i + 1:]
+                return s[i : ] == t[i + 1:]
+        return m != n
