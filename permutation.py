b2adc2ac20ed9621a8a5ef1561db1489c63f05f5
Peinan Teng <tengpn@gmail.com>
2018-03-14 11:46:12 -0700

    Address the comment

--- permutation.py
+++ permutation.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        self.res = []
-        self.helper(nums, [])
-        return self.res
-    def helper(self, nums, path):
+        res = []
+        self.helper(nums, [], res)
+        return res
+    def helper(self, nums, path, res):
         if not nums:
-            self.res.append(path)
+            res.append(path)
             return
         for i in range(len(nums)):
-            self.helper(nums[:i] + nums[i + 1:], path + [nums[i]])
+            self.helper(nums[:i] + nums[i + 1:], path + [nums[i]], res)
             
