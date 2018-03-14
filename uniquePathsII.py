d1d7fb9afcd60ef81a033a3770d6735dfe89e404
Peinan Teng <tengpn@gmail.com>
2018-03-14 11:38:49 -0700

    Address the comment

--- uniquePathsII.py
+++ uniquePathsII.py
@@ -5,25 +5,24 @@ class Solution(object):
         :rtype: int
         """
         m = len(obstacleGrid)
-        n = len(obstacleGrid[0])
-        matrix = [[0] * n for i in range(m)]
-        
-        if obstacleGrid[0][0] == 1:
+        if m == 0:
             return 0
-        x, y = 0, 0
-        matrix[x][y] = 1
-        y += 1
-        while y < n:
+        n = len(obstacleGrid[0])
+        if n == 0 or obstacleGrid[0][0] == 1:
+            return 0
+
+        matrix = [[0] * n for i in range(m)]
+        matrix[0][0] = 1
+        x = 0
+        for y in range(1, n):
             if obstacleGrid[x][y] == 0:
                 matrix[x][y] = matrix[x][y - 1]
-            y += 1
-        x, y = 1, 0
-        while x < m:
+        y = 0
+        for x in range(1, m):
             if obstacleGrid[x][y] == 0:
                 matrix[x][y] = matrix[x - 1][y]
-            x += 1
         for x in range(1, m):
             for y in range(1, n):
                 if obstacleGrid[x][y] == 0:
                     matrix[x][y] = matrix[x - 1][y] + matrix[x][y - 1]
-        return matrix[m - 1][n - 1]
+        return matrix[-1][-1]
