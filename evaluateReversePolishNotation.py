44de951e64922940d9eec932afab729e8024b245
Peinan Teng <tengpn@gmail.com>
2018-03-14 14:34:48 -0700

    Add evaluateReversePolishNotation.py

---
+++ evaluateReversePolishNotation.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+        for char in tokens:
+            if char not in ('+', '-', '*', '/'):
+                stack.append(int(char))
+            else:
+                num2 = stack.pop()
+                num1 = stack.pop()
+                if char == '+':
+                    stack.append(num1 + num2)
+                elif char == '-':
+                    stack.append(num1 - num2)
+                elif char == '*':
+                    stack.append(num1 * num2)
+                elif char == '/':
+                    stack.append(int(num1 * 1.0 / num2))
+        return stack[0]
