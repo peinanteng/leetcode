61846346a6d3c876be86ddf4844a0b1c7afc25a4
Peinan Teng <tengpn@gmail.com>
2018-03-15 13:40:55 -0700

    Address the comment

--- KthSmallestElementInASortedMatrix.py
+++ KthSmallestElementInASortedMatrix.py
@@ -9,29 +9,33 @@ class Solution(object):
             return 0
         m = len(matrix)
         n = len(matrix[0])
-        heap = []
+        heap = Heap()
         for i in range(m):
-            self.heapAdd(heap, (matrix[i][0], i, 0))
+            heap.add((matrix[i][0], i, 0))
         while k > 1:
-            minItem = self.heapPop(heap)
-            x, y = minItem[1], minItem[2]
+            _, x, y = heap.pop()
             if y + 1 < n:
-                self.heapAdd(heap, (matrix[x][y + 1], x, y + 1))
+                heap.add((matrix[x][y + 1], x, y + 1))
             k -= 1
-        return self.heapPop(heap)[0]
+        return heap.pop()[0]
     
-    def heapAdd(self, heap, item):
-        heap.append(item)
-        self.bubbleUp(heap, len(heap) - 1)
-    
-    def heapPop(self, heap):
-        length = len(heap) - 1
-        heap[0], heap[length] = heap[length], heap[0]
-        item = heap.pop()
-        self.bubbleDown(heap, 0)
+class Heap(object):
+    def __init__(self):
+        self.heap = []
+
+    def add(self, item):
+        self.heap.append(item)
+        self.bubbleUp(len(self.heap) - 1)
+
+    def pop(self):
+        heap = self.heap
+        heap[0], heap[-1] = heap[-1], heap[0]
+        item = self.heap.pop()
+        self.bubbleDown(0)
         return item
-    
-    def bubbleUp(self, heap, index):
+
+    def bubbleUp(self, index):
+        heap = self.heap
         while index > 0:
             paren = (index - 1) // 2
             if heap[index][0] < heap[paren][0]:
@@ -39,8 +43,9 @@ class Solution(object):
                 index = paren
             else:
                 break
-    
-    def bubbleDown(self, heap, index):
+
+    def bubbleDown(self, index):
+        heap = self.heap
         while index < len(heap):
             leftChild = index * 2 + 1
             rightChild = index * 2 + 2
@@ -55,4 +60,3 @@ class Solution(object):
             else:
                 heap[leftChild], heap[index] = heap[index], heap[leftChild]
                 index = leftChild
-            
