6bf280bd339bccb21eaafa7aef1b73898e3c81ff
Peinan Teng <tengpn@gmail.com>
2018-03-15 13:49:30 -0700

    Address the comment

--- oneEditDistance.py
+++ oneEditDistance.py
@@ -14,5 +14,5 @@ class Solution(object):
             if s[i] != t[i]:
                 if m == n:
                     return s[i + 1:] == t[i + 1:]
-                return s[i : ] == t[i + 1:]
+                return s[i:] == t[i + 1:]
         return m != n
