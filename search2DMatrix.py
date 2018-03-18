588f1084d640ddd6706698421c06fbad7cb9618d
Peinan Teng <tengpn@gmail.com>
2018-03-17 22:04:50 -0700

    Address the comment

--- search2DMatrix.py
+++ search2DMatrix.py
@@ -7,32 +7,30 @@ class Solution(object):
         """
         if not matrix or not matrix[0]:
             return False
-        column = 0
-        up = 0
-        down = len(matrix) - 1
-        while up + 1 < down:
-            mid = (up + down) // 2
+        top, bottom = 0, len(matrix) - 1
+        while top + 1 < bottom:
+            mid = (top + bottom) // 2
             if matrix[mid][0] < target:
-                up = mid
+                top = mid
             elif matrix[mid][0] == target:
                 return True
             else:
-                down = mid
-        if matrix[up][0] > target:
+                bottom = mid
+        if matrix[top][0] > target:
             return False
-        if matrix[up][0] == target or matrix[down][0] == target:
+        if matrix[top][0] == target or matrix[bottom][0] == target:
             return True
-        if matrix[down][0] < target:
-            up = down
-        left, right = 0, len(matrix[up]) - 1
+        if matrix[bottom][0] < target:
+            top = bottom
+        left, right = 0, len(matrix[top]) - 1
         while left + 1 < right:
             mid = (left + right) // 2
-            if matrix[up][mid] < target:
+            if matrix[top][mid] < target:
                 left = mid
-            elif matrix[up][mid] > target:
+            elif matrix[top][mid] > target:
                 right = mid
             else:
                 return True
-        if matrix[up][left] == target or matrix[up][right] == target:
+        if matrix[top][left] == target or matrix[top][right] == target:
             return True
         return False
