3dfa2e551b2845998accb2a637976ef303bdc003
Peinan Teng <tengpn@gmail.com>
2018-03-17 22:17:22 -0700

    Address the comment

--- validSudoku.py
+++ validSudoku.py
@@ -1,29 +1,34 @@
+class Solution:
+    """
+    @param board: the board
+    @return: whether the Sudoku is valid
+    """
+    import math
+    def isValidSudoku(self, board):
+        # write your code here
         m = len(board)
         l = int(math.sqrt(m))
         for i in range(m):
             summary = {}
             for j in range(m):
                if board[i][j] != '.':
-                    summary[board[i][j]] = summary.get(board[i][j], 0) + 1
-            for key, val in summary.items():
-                if val > 1:
-                    return False
+                    if board[i][j] in summary:
+                        return False
+                    summary[board[i][j]] = 1
         for j in range(m):
             summary = {}
             for i in range(m):
                 if board[i][j] != '.':
-                    summary[board[i][j]] = summary.get(board[i][j], 0) + 1
-            for key, val in summary.items():
-                if val > 1:
-                    return False
+                    if board[i][j] in summary:
+                        return False
+                    summary[board[i][j]] = 1
         for squareX in range(l):
             for squareY in range(l):
                 summary = {}
-                for i in range(squareX *l , (squareX + 1) * l):
-                    for j in range(squareY*l, (squareY + 1) * l):
+                for i in range(squareX * l , (squareX + 1) * l):
+                    for j in range(squareY * l, (squareY + 1) * l):
                         if board[i][j] != '.':
-                            summary[board[i][j]] = summary.get(board[i][j], 0) + 1
-                for key, val in summary.items():
-                    if val > 1:
-                        return False
+                            if board[i][j] in summary:
+                                return False
+                            summary[board[i][j]] = 1
         return True
