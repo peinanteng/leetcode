6fd9c7b24f7d56104a6fb08bd287ec5ce1f857b3
Peinan Teng <tengpn@gmail.com>
2018-03-17 22:21:27 -0700

    Address the comment

--- repeatedDNASequences.py
+++ repeatedDNASequences.py
@@ -9,7 +9,8 @@ class Solution(object):
             return res
         summary = {}
         for i in range(len(s) - 9):
-            if s[i:i+ 10] in summary and summary[s[i:i+10]] == 1:
-                    res.append(s[i:i+10])
-            summary[s[i:i+10]] = summary.get(s[i:i+10], 0) + 1
+            string = s[i : i + 10]
+            if string in summary and summary[string] == 1:
+                res.append(string)
+            summary[string] = summary.get(string, 0) + 1
         return res
