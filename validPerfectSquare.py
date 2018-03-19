5786523956c798eb702af65a3611d0eb20aeca3d
Peinan Teng <tengpn@gmail.com>
2018-03-19 16:35:18 -0700

    Add validPerfectSquare.py

---
+++ validPerfectSquare.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        left, right = 1, num
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if mid * mid == num:
+                return True
+            elif mid * mid < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        if left * left == num or right * right == num:
+            return True
+                
+        return False
