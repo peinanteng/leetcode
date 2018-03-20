38f126cebc55bfb0e1611ffda44b75d8743d3f7c
Peinan Teng <tengpn@gmail.com>
2018-03-19 17:11:15 -0700

    Modify factorCombination.py

--- factorCombinations.py
+++ factorCombinations.py
@@ -1,5 +1,4 @@
 class Solution(object):
-    import math
     def getFactors(self, n):
         """
         :type n: int
@@ -7,18 +6,28 @@ class Solution(object):
         """
         if n <= 3:
             return []
+        m = int(n ** 0.5)
         res = []
-        m = int(math.sqrt(n))
         for i in range(2, m + 1):
             if n % i == 0:
-                num1, num2 = i, n // i
-                res.append([num1, num2])
-                res2 = self.getFactors(num2)
-                if not res2:
-                    continue
-                else:
-                    for list2 in res2:
-                        if min(list2) >= num1:
-                            res += [[num1] + list2]
+                num1 = i
+                nums2 = self.helper(i, n // i)
+                for res2 in nums2:
+                    res.append([num1] + res2)
+        return res
+    
+    def helper(self, startNum, num):
+        if num < startNum:
+            return []
+        res = [[num]]
+        if num < startNum * startNum:
+            return res
+        m = int(num ** 0.5)
+        for i in range(startNum, m + 1):
+            if num % i == 0:
+                num1 = i
+                nums2 = self.helper(i, num // i)
+                for res2 in nums2:
+                    res.append([num1] + res2)
         return res
         
