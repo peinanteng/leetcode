be8731344d71e0fe2e81391615f85211ddc35a08
Peinan Teng <tengpn@gmail.com>
2018-03-21 21:49:32 -0700

    Add graphValidTree.py

--- graphValidTree.py
+++ graphValidTree.py
@@ -5,20 +5,18 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: bool
         """
-        if len(edges) != n - 1:
-            return False
-        summary = {}
+        
+        root = [i for i in range(n)]
         for edge in edges:
-            summary[edge[0]] = summary.get(edge[0], []) + [edge[1]]
-            summary[edge[1]] = summary.get(edge[1], []) + [edge[0]]
-        visited = {}
-        visited[0] = True
-        queue = [0]
-        while queue:
-            cur = queue.pop(0)
-            if cur in summary:
-                for node in summary[cur]:
-                    if node not in visited:
-                        visited[node] = True
-                        queue.append(node)
-        return len(visited) == n
+            root1 = self.find(root, edge[0])
+            root2 = self.find(root, edge[1])
+            if root1 == root2:
+                return False
+            root[root2] = root1
+        return len(edges) == n - 1
+    
+    def find(self, root, e):
+        if root[e] == e:
+            return e
+        root[e] = self.find(root, root[e])
+        return root[e]
