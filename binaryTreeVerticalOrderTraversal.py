42d0b7e506f508e103f831d26ed35b963d498009
Peinan Teng <tengpn@gmail.com>
2018-03-23 17:28:40 -0700

    Address the comment

--- binaryTreeVerticalOrderTraversal.py
+++ binaryTreeVerticalOrderTraversal.py
@@ -5,36 +5,37 @@
 #         self.left = None
 #         self.right = None
 
+from collections import deque
 class Solution(object):
     def verticalOrder(self, root):
         """
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        from collections import deque
         if not root:
             return []
-        index = - self.helper(root, 0)
-        res = [[] for _ in range(index + 1)]
-        dq = deque([[index, root]])
+        i = - self.helper(root, 0)
+        res = [[] for _ in range(i + 1)]
+        dq = deque([[i, root]])
         while dq:
             item = dq.popleft()
-            i, node = item[0], item[1]
-            if i >= len(res):
+            j, node = item[0], item[1]
+            if j >= len(res):
                 res.append([node.val])
             else:
-                res[i].append(node.val)
+                res[j].append(node.val)
             if node.left:
-                dq.append([i - 1, node.left])
+                dq.append([j - 1, node.left])
             if node.right:
-                dq.append([i + 1, node.right])
+                dq.append([j + 1, node.right])
             
         return res
     
-    def helper(self, root, index):
-        left, right = index, index
+    def helper(self, root, i):
+        left, right = i, i
         if root.left:
-            left = self.helper(root.left, index - 1)
+            left = self.helper(root.left, i - 1)
         if root.right:
-            right = self.helper(root.right, index + 1)
-        return min(left, right, index)
+            right = self.helper(root.right, i + 1)
+        return min(left, right, i)
+
