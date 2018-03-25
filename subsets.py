b97c378a0661297807edfb1a9add25bd67a31cd3
Peinan Teng <tengpn@gmail.com>
2018-03-25 13:22:38 -0700

    Modify subsets.py

--- subsets.py
+++ subsets.py
@@ -1,12 +1,13 @@
-class Solution():
-    def Subsets(self, nums):
-        self.res = []
-        self.dfs(nums, [], 0)
-        return self.res
-    def dfs(self, nums, curpath, startIndex):
+class Solution(object):
+    def subsets(self, nums):
+        res = []
+        self.dfs(nums, [], 0, res)
+        return res
+    
+    def dfs(self, nums, cur, startIndex, res):
         if startIndex == len(nums):
-            self.res.append(curpath)
+            res.append(cur)
             return
-        self.dfs(nums, curpath, startIndex + 1)
-        self.dfs(nums, curpath + [nums[startIndex]], startIndex + 1)
+        self.dfs(nums, cur, startIndex + 1, res)
+        self.dfs(nums, cur + [nums[startIndex]], startIndex + 1, res)
 
