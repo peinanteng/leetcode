5b4ed074f7642cb0a632fb6a32c9c17e7afab39e
Peinan Teng <tengpn@gmail.com>
2018-03-25 21:59:14 -0700

    Modify permutation.py

--- permutationII.py
+++ permutationII.py
@@ -4,16 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        self.res = []
+        res = []
         nums.sort()
-        self.helper(nums, [])
-        return self.res
-    def helper(self, nums, path):
+        self.helper(nums, [], res)
+        return res
+    
+    def helper(self, nums, cur, res):
         if not nums:
-            self.res.append(path)
+            res.append(cur)
+            return
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            self.helper(nums[:i] + nums[i + 1:], path +[nums[i]])
+            self.helper(nums[:i] + nums[i + 1:], cur + [nums[i]], res)
     
             
