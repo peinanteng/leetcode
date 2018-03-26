11295246370d1d3ddff5bca729dd0c982d2a0e09
Peinan Teng <tengpn@gmail.com>
2018-03-25 22:02:30 -0700

    Address the comment

--- removeDuplicatesFromSortedArrayII.py
+++ removeDuplicatesFromSortedArrayII.py
@@ -4,17 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) == 0:
+        if len(nums) <= 1:
             return 0
-        if len(nums) == 1:
-            return 1
         tail = 1
-        prePre, pre = nums[0], nums[1]
+        pre, cur = nums[0], nums[1]
         for i in range(2, len(nums)):
-            if nums[i] == prePre and nums[i] == pre:
+            if nums[i] == pre and nums[i] == cur:
                 continue
             tail += 1
-            prePre, pre = pre, nums[i]
+            pre, cur = cur, nums[i]
             nums[tail], nums[i] = nums[i], nums[tail]
         return tail + 1
         
