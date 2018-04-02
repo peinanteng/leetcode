d089e2b3fe4e112888b61e89f802cfa3cc838a77
Peinan Teng <tengpn@gmail.com>
2018-04-02 15:50:48 -0700

    Add findPeakElement.py

---
+++ findPeakElement.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        start, end = 0, len(nums) - 1
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if nums[mid] > nums[mid - 1]:
+                start = mid
+            elif nums[mid] > nums[mid + 1]:
+                end = mid
+            else:
+                end = mid
+        if nums[start] < nums[end]:
+            return end
+        return start
